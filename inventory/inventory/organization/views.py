f54b8b747d04752e3cf10cee7141d2163743df37
Anton Petrov <a.petrov.83@gmail.com>
2024-03-31 11:29:29 +0300

    Made all form display flex and made correction on styling.

--- inventory/inventory/organization/views.py
+++ inventory/inventory/organization/views.py
@@ -1,19 +1,28 @@
 from django.urls import reverse_lazy
 from django.views import generic as views
-
+from django.contrib.auth import mixins as auth_mixins
 from inventory.organization.forms import OrganizationCreateForm, OrganizationEditForm
 from inventory.organization.models import Organization
 
 
-class CreateOrganizationView(views.CreateView):
+class CreateOrganizationView(auth_mixins.LoginRequiredMixin, views.CreateView):
     queryset = Organization.objects.all()
     template_name = 'organization/create-organization-page.html'
     form_class = OrganizationCreateForm
 
-    success_url = reverse_lazy('home-page')
+    def get_template_names(self):
+        user = self.request.user
+        organization = Organization.objects.exists()
+
+        if user.is_superuser and organization:
+            template_name = 'organization/organization-created.html'
+        else:
+            template_name = self.template_name
+
+        return [template_name]
 
 
-class EditOrganizationView(views.UpdateView):
+class EditOrganizationView(auth_mixins.LoginRequiredMixin, views.UpdateView):
     queryset = Organization.objects.all()
     form_class = OrganizationEditForm
     template_name = 'organization/edit-organization-page.html'
