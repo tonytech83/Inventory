1fa0a7266050d50360b8e76d0876da84f662944a
Anton Petrov <a.petrov.83@gmail.com>
2024-04-02 19:02:49 +0300

    Update settings.py

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
