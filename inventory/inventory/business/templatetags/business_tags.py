2d83817187f7b7b469cbd6e7d92bee64f85986c8
tonytech83 <a.petrov.83@gmail.com>
2024-04-06 09:45:35 +0300

    Done some enchantments.

--- inventory/inventory/business/templatetags/business_tags.py
+++ inventory/inventory/business/templatetags/business_tags.py
@@ -7,31 +7,35 @@ from django.utils.timezone import now
 register = template.Library()
 
 
+# Support
+def count_devices_by_filter(business, filter_query=None):
+    if isinstance(business, QuerySet):
+        return sum(
+            [b.device_set.filter(filter_query).count() if filter_query else b.device_set.count()
+             for b in business]
+        )
+
+    return business.device_set.filter(filter_query).count() if filter_query else business.device_set.count()
+
+
 @register.simple_tag
 def count_no_support_devices(business):
-    if isinstance(business, QuerySet):
-        return sum([b.device_set.filter(eos__lt=now().date()).count() for b in business])
-
-    return business.device_set.filter(eos__lt=now().date()).count()
+    filter_query = Q(eos__lt=now().date())
+    return count_devices_by_filter(business, filter_query)
 
 
 @register.simple_tag
 def count_devices_in_support(business):
     today = now().date()
     one_year_from_now = today + timedelta(days=365)
-
-    if isinstance(business, QuerySet):
-        return sum([b.device_set.filter(eos__gt=one_year_from_now).count() for b in business])
-
-    return business.device_set.filter(eos__gt=one_year_from_now).count()
+    filter_query = Q(eos__gt=one_year_from_now)
+    return count_devices_by_filter(business, filter_query)
 
 
 @register.simple_tag
 def count_devices_unknown_support(business):
-    if isinstance(business, QuerySet):
-        return sum([b.device_set.filter(eos=None).count() for b in business])
-
-    return business.device_set.filter(eos=None).count()
+    filter_query = Q(eos=None)
+    return count_devices_by_filter(business, filter_query)
 
 
 @register.simple_tag
@@ -39,13 +43,8 @@ def count_lt_year_gt_six_month(business):
     today = now().date()
     six_months_from_now = today + timedelta(days=182)
     one_year_from_now = today + timedelta(days=365)
-
-    query = Q(eos__gt=six_months_from_now) & Q(eos__lt=one_year_from_now)
-
-    if isinstance(business, QuerySet):
-        return sum([b.device_set.filter(query).count() for b in business])
-
-    return business.device_set.filter(query).count()
+    filter_query = Q(eos__gt=six_months_from_now) & Q(eos__lt=one_year_from_now)
+    return count_devices_by_filter(business, filter_query)
 
 
 @register.simple_tag
@@ -53,28 +52,19 @@ def count_lt_six_gt_three_months(business):
     today = now().date()
     six_months_from_now = today + timedelta(days=182)
     three_months_from_now = today + timedelta(days=90)
-
-    query = Q(eos__gt=three_months_from_now) & Q(eos__lt=six_months_from_now)
-
-    if isinstance(business, QuerySet):
-        return sum([b.device_set.filter(query).count() for b in business])
-
-    return business.device_set.filter(query).count()
+    filter_query = Q(eos__gt=three_months_from_now) & Q(eos__lt=six_months_from_now)
+    return count_devices_by_filter(business, filter_query)
 
 
 @register.simple_tag
 def count_lt_three_months_and_no_support(business):
     today = now().date()
     three_months_from_now = today + timedelta(days=90)
-
-    query = Q(eos__gt=today) & Q(eos__lt=three_months_from_now)
-
-    if isinstance(business, QuerySet):
-        return sum([b.device_set.filter(query).count() for b in business])
-
-    return business.device_set.filter(query).count()
+    filter_query = Q(eos__gt=today) & Q(eos__lt=three_months_from_now)
+    return count_devices_by_filter(business, filter_query)
 
 
+# Not reviewed devices
 @register.simple_tag
 def count_no_reviewed_devices(business):
     devices = business.device_set.all()
@@ -82,6 +72,7 @@ def count_no_reviewed_devices(business):
     return sum(not device.is_reviewed for device in devices)
 
 
+# Status
 @register.filter(name='get_query_param')
 def get_query_param(value, arg):
     """Fetch a query parameter value by its name."""
@@ -96,6 +87,7 @@ def count_by_status(business, status):
     return business.device_set.filter(status=status).count()
 
 
+# Category
 @register.simple_tag
 def count_by_category(business, category):
     if isinstance(business, QuerySet):
